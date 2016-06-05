54e8b8197166ef594a6093d7833556165952c39a
regisverdin <regisverdin@gmail.com>
2016-06-04 22:04:03 -0400

    more 2 col

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
+
+
   $('.slides').slick({
 	  slidesToShow: 1,
 	  slidesToScroll: 1,
 	  dots: true,
-	  arrows: true
+	  arrows: true,
+	  fade:true
   });
+
+
+  $('.navItem').on('click', function(){
+
+    var index = $(this).attr("data-index");
+    console.log(index);
+
+  	$('.slides').slick('slickGoTo', index);
+
+  });
+
+
 
 
 
