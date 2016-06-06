fd42f1514a422eea04819a29327c35a5f4d22545
regisverdin <regisverdin@gmail.com>
2016-06-05 23:21:46 -0400

    more pics etc.

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 $(document).ready(function(){
 
+    $('.slides').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        fade:true,
+        accessibility:true,
+        waitForAnimate:false,
+        adaptiveHeight:true
+    });
 
-  $('.slides').slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  dots: true,
-	  arrows: true,
-	  fade:true
-  });
-
-
-  $('.navItem').on('click', function(){
-
-    var index = $(this).attr("data-index");
-    console.log(index);
-
-  	$('.slides').slick('slickGoTo', index);
-
-  });
-
-
-
-
-
+    $('.navItem').on('click', function(){
+        var index = $(this).attr("data-index");
+        $('.slides').slick('slickGoTo', index);
+    });
 
 });
